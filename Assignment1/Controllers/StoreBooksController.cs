ec517885e9d48bf8f22127c3746629a504f944da
Vo-Ba-Nguyen <80742814+Vo-Ba-Nguyen@users.noreply.github.com>
2022-05-10 13:51:11 +0700

    Add Controller StoreBooks to manage books in store of seller

---
+++ Assignment1/Controllers/StoreBooksController.cs
@@ -0,0 +1,218 @@
+﻿using Assignment1.Areas.Identity.Data;
+using Assignment1.Data;
+using Assignment1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment1.Controllers
+{
+    [Authorize(Roles = "Seller")]
+    public class StoreBooksController : Controller
+    {
+        private readonly UserContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly int _recordsPerPage = 5;
+        private readonly int _recordsPerPages = 30;
+        private readonly UserManager<Assignment1User> _userManager;
+
+        public StoreBooksController(UserContext context, UserManager<Assignment1User> userManager, IEmailSender emailSender)
+        {
+            _context = context;
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Seller")]
+        public async Task<IActionResult> Index(int id, string searchString)
+        {
+            Assignment1User thisUser = await _userManager.GetUserAsync(HttpContext.User);
+
+            var thisStore = await _context.Store
+                .FirstOrDefaultAsync(s => s.UserId == thisUser.Id);
+
+            if (thisStore == null)
+                return RedirectToAction("Create", "Stores");
+
+            var books = _context.Book
+                .Where(b => b.StoreId == thisStore.Id);
+
+
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                books = books.Where(s => s.Title.Contains(searchString));
+            }
+
+            var model = await books.ToListAsync();
+
+            return View(model);
+        }
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .Include(b => b.Store)
+                .FirstOrDefaultAsync(m => m.Isbn == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Seller")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IFormFile image, [Bind("Isbn,Title,Pages,Author,Category,Price,Desc,ImgUrl")] Book book)
+        {
+            if (image != null)
+            {
+                //set key name
+                string ImageName = book.Isbn + Path.GetExtension(image.FileName);
+
+                string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Image", ImageName);
+                using (var stream = new FileStream(SavePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                book.ImgUrl = "img/" + ImageName;
+                Assignment1User thisUser = await _userManager.GetUserAsync(HttpContext.User);
+                Store thisStore = await _context.Store.FirstOrDefaultAsync(s => s.UserId == thisUser.Id);
+                book.StoreId = thisStore.Id;
+            }
+            else
+            {
+                return View(book);
+            }
+            _context.Add(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "Id", book.StoreId);
+
+            return View(book);
+        }
+        [Authorize(Roles = "Seller")]
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "Id", book.StoreId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Seller")]
+        public async Task<IActionResult> Edit(string id, [Bind("Isbn,Title,Pages,Author,Category,Price,Desc,ImgUrl")] Book book)
+        {
+            if (id != book.Isbn)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var bookToUpdate = await _context.Book.FirstOrDefaultAsync(s => s.Isbn == id);
+                if (bookToUpdate == null)
+                {
+                    return NotFound();
+                }
+                bookToUpdate.Title = book.Title;
+                bookToUpdate.Pages = book.Pages;
+                bookToUpdate.Category = book.Category;
+                bookToUpdate.Author = book.Author;
+                bookToUpdate.Pages = book.Pages;
+                bookToUpdate.Price = book.Price;
+                bookToUpdate.Desc = book.Desc;
+                try
+                {
+                    _context.Update(bookToUpdate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    ModelState.AddModelError("", "Unable to update the change. Error is: " + ex.Message);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "Id", book.StoreId);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        [Authorize(Roles = "Seller")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .Include(b => b.Store)
+                .FirstOrDefaultAsync(m => m.Isbn == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [Authorize(Roles = "Seller")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            try
+            {
+                _context.Book.Remove(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError("", "Unable to delete book " + id + ". Error is: " + ex.Message);
+                return NotFound();
+
+            }
+        }
+
+    }
+}
